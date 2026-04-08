9c23315962a5d2915b9580a70b6658421953a4e7
yash2607 <yashaswi2607@gmail.com>
2026-04-08 23:17:55 +0530

    Fix SMTP config and deploy workflow

--- mailer.js
+++ mailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // Create a transporter object using Zoho's SMTP transport
 const transporter = nodemailer.createTransport({
-  host: 'smtppro.zoho.in',
+  host: 'smtp.zoho.in',
   port: 465,
   secure: true, // use SSL
   auth: {
